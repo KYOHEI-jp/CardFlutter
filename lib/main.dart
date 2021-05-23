310d8df428a3413f6dfe5a096a72710fe14ebaba
渡辺恭平 <kingdomhearts510@gmail.com>
2021-05-23 19:25:13 +0900

    待ち受けっぽいの作った

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,20 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('Images/prac.png'),
               ),
-              Container(
-                width: 100,
-                height: 400,
-                color: Colors.blue,
-              ),
-              Container(
-                child: Text("container 3"),
-                width: 300,
-                height: 100,
-                color: Colors.yellow,
+              Text(
+                "hello wallpaper",
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
               )
             ],
-          ),
-        ),
+          )),
       ),
     );
   }
