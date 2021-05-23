5b66057a7e1f355126bdae2a46236cee74e06d7e
渡辺恭平 <kingdomhearts510@gmail.com>
2021-05-23 20:33:14 +0900

    電話番号とEmailOK

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,58 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('Images/prac.png'),
-              ),
-              Text(
-                "hello wallpaper",
-                style: TextStyle(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('Images/prac.png'),
+            ),
+            Text(
+              "hello wallpaper",
+              style: TextStyle(
                   fontSize: 40,
                   color: Colors.white,
-                  fontWeight: FontWeight.bold
-                ),
-              )
-            ],
-          )),
+                  fontWeight: FontWeight.bold),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.phone, color: Colors.teal),
+                  SizedBox(width: 10.0),
+                  Text(
+                    "123 456 789",
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source Sans Pro",
+                        fontSize: 20.0),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    Text(
+                      "kyohei@gmail.com",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: "Source Sans Pro"),
+                    )
+                  ],
+                ))
+          ],
+        )),
       ),
     );
   }
